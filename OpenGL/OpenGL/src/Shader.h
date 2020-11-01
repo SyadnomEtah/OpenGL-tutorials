5b3667edc0079a233bc642246745bbef34d40a85
Arkadiusz Wiśniewski <syadnom.etah@gmail.com>
2020-11-01 23:29:23 +0100

    Added glm math library

--- OpenGL/OpenGL/src/Shader.h
+++ OpenGL/OpenGL/src/Shader.h
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <unordered_map>
 
+#include "glm/glm.hpp"
+
 struct ShaderProgramSource
 {
 	std::string VertexSource;
@@ -29,6 +31,7 @@ public:
 	//Set uniforms
 	void SetUniform1i(const std::string& name, int value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniformMat4f(const std::string& name, const glm::mat4 matrix);
 
 private:
 	ShaderProgramSource ParseShader(const std::string& filepath);
